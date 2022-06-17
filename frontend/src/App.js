605fee684a1d7aadf0a7c3c67f52e23f1a0c1ffc
YUNGSHENG <pupugggg@gmail.com>
2022-06-17 21:30:02 +0800

    WIP:sign up site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,69 @@
 import './App.css'
 import { io } from 'socket.io-client'
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import Peer from 'simple-peer'
-import {Box} from '@mui/material'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes
-  ,
-} from "react-router-dom";
-import SignInSide from './components/main'
+import { Box } from '@mui/material'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import SignInSide from './components/Login'
+import RegisterSide from './components/Register'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#3f51b5',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"',
+        ].join(','),
+    },
+})
 function App() {
-    const [s,setS] = useState(null)
+    const [s, setS] = useState(null)
     useEffect(() => {
         const socket = io('http://localhost:5000')
         setS(socket)
-        socket.emit('join','r1')
-        socket.on('joined',(room,socketID)=>{
-          console.log('joined',room,socketID)
+        socket.emit('join', 'r1')
+        socket.on('joined', (room, socketID) => {
+            console.log('joined', room, socketID)
         })
         socket.on('leave', (room, socketID) => {
-            console.log('leave',room, socketID)
+            console.log('leave', room, socketID)
         })
 
-        socket.on('disconnect',(reason)=>{
-          console.log(reason)
+        socket.on('disconnect', (reason) => {
+            console.log(reason)
         })
         return () => {
-            socket.emit('leave','r1')
+            socket.emit('leave', 'r1')
         }
     }, [])
-    return <Box>
-      <Router>
-        <Routes>
-        <Route path='*' element={<SignInSide/>}/>
-        <Route path='/room/:id' element={<SignInSide/>}/>
-        </Routes>
-      </Router>
-    </Box>
+    return (
+        <ThemeProvider theme={theme}>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<SignInSide />} />
+                    <Route path="/register" element={<RegisterSide />} />
+                    <Route path="*" element={<SignInSide />} />
+                    <Route path="/room/:id" element={<SignInSide />} />
+                </Routes>
+            </Router>
+        </ThemeProvider>
+    )
 }
 
 export default App
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,15 +3,12 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {useDispatch} from 'react-redux'
 import {reset} from '../features/room/roomSlice'
 function Copyright(props) {
@@ -27,8 +24,6 @@ function Copyright(props) {
   );
 }
 
-const theme = createTheme();
-
 export default function SignInSide() {
     const dispatch = useDispatch()
   const handleSubmit = (event) => {
@@ -43,7 +38,7 @@ export default function SignInSide() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
+  
       <Grid container component="main" sx={{ height: '100vh' }}>
         <CssBaseline />
         <Grid
@@ -97,10 +92,6 @@ export default function SignInSide() {
                 id="password"
                 autoComplete="current-password"
               />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
               <Button
                 type="submit"
                 fullWidth
@@ -110,13 +101,8 @@ export default function SignInSide() {
                 Sign In
               </Button>
               <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
@@ -126,6 +112,5 @@ export default function SignInSide() {
           </Box>
         </Grid>
       </Grid>
-    </ThemeProvider>
   );
 }
